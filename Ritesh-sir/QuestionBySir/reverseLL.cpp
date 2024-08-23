22ab36cf2769190ae6f1fd151253e5f477265d66
aryanwalia24 <aryanwalia2303@gmail.com>
2024-08-23 20:42:38 +0530

    Class Questions -Ritesh sir

---
+++ Ritesh-sir/QuestionBySir/reverseLL.cpp
@@ -0,0 +1,23 @@
+ListNode reverseList(ListNode head) 
+    {
+        // ListNode prev = null, curr = head;
+        // while(curr != null)
+        // {
+        //     ListNode next = curr.next;
+        //     curr.next = prev;
+        //     prev = curr;
+        //     curr = next;
+        // }
+
+        // return prev;
+
+        if(head == null || head.next == null)
+        {
+            return head;
+        }
+
+        ListNode newNode = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newNode;
+    }
